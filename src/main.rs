7ac84990e6214d595ac300600c2ffb6f4e18a572
nicolas4pitz <nicolas2004pitz@gmail.com>
2024-11-05 11:57:44 -0300

    Arquivo sendo buscado

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
+use anyhow::{Context, Result};
 use clap::Parser;
-use std::io::{BufRead, BufReader};
-use std::fs::File;
-use std::result;
 
 #[derive(Parser)]
 struct Cli {
@@ -9,27 +7,15 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    // let file = File::open(&args.path).expect("could not open file"); TEntei
-    // let result = BufReader::new(file);
-
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    let result = std::fs::read_to_string("test.txt");
-    let content = match result {
-        Ok(content) => { content },
-        Err(error) => { panic!("Can't deal with {}, just exit here", error); } };
+    let path = "test.txt";
+    let content =
+        std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
     println!("file content: {}", content);
-    // for line in result.lines() {
-    //     let line = line.expect("could not read line");
-    //     if line.contains(&args.pattern){
-    //         println!("{}", line);
-    //     }
-    // }
-
-    for line in content.lines(){ //Basicamnete, ele le o arquivo linha por linha
-        if line.contains(&args.pattern){ //Se a linha contiver o padrao, ele imprime a linha
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
